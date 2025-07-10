e398dc701dba6f0d6da2363b4f2c03c7c00db3ce
cristiooney <joao.santos83@alunos.sc.senac.br>
2025-07-10 11:25:04 -0300

    update 2 joao
    eu mudei a bolinha

--- Education-Advocates/Assets/Scripts/Playermove.cs
+++ Education-Advocates/Assets/Scripts/Playermove.cs
@@ -43,7 +43,7 @@ public class Playermove : MonoBehaviour
         }
 
         // Aplica movimento
-        body.velocity = new Vector2(horizontal, vertical) * speed;
+        body.linearVelocity = new Vector2(horizontal, vertical) * speed;
 
         // Disparo
         if (Input.GetButtonDown("Fire1"))
