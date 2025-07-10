8e52876caf1eeb2950ee75662a8d14cbdfbe8549
Vithenzofeldkircher <vithenzo.feldkircher@alunos.sc.senac.br>
2025-07-10 11:17:48 -0300

    Update Playermove.cs

--- Education-Advocates/Assets/Scripts/Playermove.cs
+++ Education-Advocates/Assets/Scripts/Playermove.cs
@@ -1,70 +1,56 @@
 using UnityEngine;
 
 public class Playermove : MonoBehaviour
-
 {
-
     private float horizontal;
-
     private float vertical;
 
     public float speed;
-
     public GameObject bullet;
 
-    Rigidbody2D body;//Necessário para programar física
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D body;
 
     void Start()
-
     {
-
-        //Pega o componente que está no mesmo GameObject
-
         body = GetComponent<Rigidbody2D>();
-
     }
 
-    // Update is called once per frame
-
     void Update()
-
     {
+        // Zera o movimento a cada frame
+        horizontal = 0;
+        vertical = 0;
+
+        // Movimento com WASD
         if (Input.GetKey(KeyCode.A))
         {
-            // Move pra esquerda
             horizontal = -1;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            // Move pra direita
             horizontal = 1;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             vertical = -1;
-
         }
+
         if (Input.GetKey(KeyCode.W))
         {
             vertical = 1;
         }
-        //Input.GetKeyDown(KeyCode.Escape);
 
-        body.linearVelocity = new Vector2(horizontal, vertical) * speed;
+        // Aplica movimento
+        body.velocity = new Vector2(horizontal, vertical) * speed;
 
+        // Disparo
         if (Input.GetButtonDown("Fire1"))
-
         {
-
             Instantiate(bullet, transform.position, transform.rotation);
-
         }
-
     }
-
 }
+
 
