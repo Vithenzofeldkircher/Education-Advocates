9ab4155048e7f8ecf22094a420e619be9b0477ab
Victor-A-A-Lima <victor.lima2@alunos.sc.senac.br>
2025-08-19 15:45:31 -0300

    Update 2 (Dialogo)
    fiz mais scripts e progredi um pouco

---
+++ Education-Advocates/Assets/Scripts/DialogueData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public struct Dialogue
+{
+    public string name;
+    [TextArea(5, 10)]
+    public string text;
+}
+
+[CreateAssetMenu(fileName = "DialogueData", menuName = "ScriptableObject/TalkScript", order = 1)]
+public class DialogueData : ScriptableObject
+{
+    public List<Dialogue> talkScript;
+}
