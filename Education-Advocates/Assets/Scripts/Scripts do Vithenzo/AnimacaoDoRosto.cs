a68c09dc9ab1f613d84f392cbe569e5fed1b320c
Vithenzofeldkircher <vithenzo.feldkircher@alunos.sc.senac.br>
2025-11-18 13:49:40 -0300

    Update(vithenzo)
    limpa

---
+++ Education-Advocates/Assets/Scripts/Scripts do Vithenzo/AnimacaoDoRosto.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimacaoDoRosto : MonoBehaviour
+{
+    [Header("animação")]
+    public Image
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
